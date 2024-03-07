65e8f0d9c5aa749fce2dd51785d2f575e8ce05cd
CelticAlreadyUse <wahyusantosokanisius@gmail.com>
2024-03-08 00:03:17 +0700

    adding information to carCard details

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -61,6 +61,44 @@ const CarDetails = ({isOpen,closeModal,car}:cardDetailProps) => {
              <Image src='/hero.png'alt='car-model'  fill 
              priority className='object-contain'/>
                     </div>
+                    <div className="flex gap-3">
+                        <div className="flex-1 relative w-full h-24 bg-primary-blue-100
+                        rounded-lg">
+                        <Image src='/hero.png'alt='car-model'  fill 
+             priority className='object-contain'/>
+                        </div>
+                        <div className="flex-1 relative w-full h-24 bg-primary-blue-100
+                        rounded-lg">
+                        <Image src='/hero.png'alt='car-model'  fill 
+             priority className='object-contain'/>
+                        </div>
+                        <div className="flex-1 relative w-full h-24 bg-primary-blue-100
+                        rounded-lg">
+                        <Image src='/hero.png'alt='car-model'  fill 
+             priority className='object-contain'/>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <div className="flex-1 flex gap-2 flex-col
+                ">
+                    <h2 className="font-semibold text-xl capitalize">
+                        {car.make} {car.model}
+                        </h2>
+                        <div className="mt-3 flex flex-wrap gap-4">
+                        {Object.entries(car).map(([key,value])=>(
+                            <div className="flex justify-between gap-5 w-full text-right"
+                            key={key}>
+                                <h4 
+                                className="text-grey capitalize"
+                                >{key.split('_').join(" ")}</h4>
+                                <p className="text-black-100 
+                                font-semibold">{value}</p>
+
+                            </div>
+                        ))}
+                        </div>
                 </div>
             </Dialog.Panel>
         </Transition.Child>
